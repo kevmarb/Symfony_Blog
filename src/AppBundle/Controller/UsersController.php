a79585905eca0ab8ea571c14598e339598f431eb
kevmarb <kevin.marburger26@gmail.com>
2015-04-04 16:26:28 +0200

    new commit

--- src/AppBundle/Controller/UsersController.php
+++ src/AppBundle/Controller/UsersController.php
@@ -2,9 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AppBundle\Entity\Users;
+use AppBundle\Form\UsersType;
 
 class UsersController extends Controller
 {
